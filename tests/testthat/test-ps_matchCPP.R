2afb15e36b35a12277fd8c3e297dc74afa61bc14
Sophie Huebler <sophhuebler@gmail.com>
2024-11-19 11:54:46 -0700

    ps_matchCPP runs but fails

---
+++ tests/testthat/test-ps_matchCPP.R
@@ -0,0 +1,19 @@
+test_that("ps works", {
+  set.seed(1231)
+  x <- cbind(runif(20))
+
+  ps_matchR <- function(x) {
+
+    match_expected <- dist(x) |> as.matrix()
+    diag(match_expected) <- .Machine$integer.max
+    indices <- apply(match_expected, 1, which.min)
+
+    list(
+      match_id = as.integer(unname(indices)),
+      match_x  = x[indices]
+    )
+
+  }
+
+  expect_equal(ps_matchR(x), ps_matchCPP(x))
+})
